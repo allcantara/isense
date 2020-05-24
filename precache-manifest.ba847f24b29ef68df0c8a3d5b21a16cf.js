c0ff73a38e18209fddc2e34e74cb610a1d1305eb
allcantara <brunoalcantarajc@gmail.com>
2020-05-24 18:12:28 -0300

    Updates

---
+++ precache-manifest.ba847f24b29ef68df0c8a3d5b21a16cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d95ea9733e7fab5d02d91dd5b8feeb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8d33df18121e1243d81",
+    "url": "/static/css/main.4e9035d3.chunk.css"
+  },
+  {
+    "revision": "88c2f82431b7d5d3817d",
+    "url": "/static/js/2.8272a5d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8272a5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d33df18121e1243d81",
+    "url": "/static/js/main.42e04222.chunk.js"
+  },
+  {
+    "revision": "1c493b2df3181dcb3317",
+    "url": "/static/js/runtime-main.a02f2a0d.js"
+  },
+  {
+    "revision": "0a5b5c55b73f577ff4ac8c9c31b4c183",
+    "url": "/static/media/aparaj.0a5b5c55.ttf"
+  },
+  {
+    "revision": "861d4bdd60a62e0c2cec0a6e91769d43",
+    "url": "/static/media/audio.861d4bdd.mp3"
+  },
+  {
+    "revision": "e029cff5f1a975adac2e8768efe47115",
+    "url": "/static/media/bg.e029cff5.jpg"
+  },
+  {
+    "revision": "cec4d8e70fd0581e461dd509f234e37c",
+    "url": "/static/media/gif1.cec4d8e7.gif"
+  },
+  {
+    "revision": "fef9453ab699047cfe56de7a009545c6",
+    "url": "/static/media/login.fef9453a.gif"
+  },
+  {
+    "revision": "6224ca361ecbfc72732acbe9959e4a0c",
+    "url": "/static/media/logo.6224ca36.png"
+  },
+  {
+    "revision": "b1644e5d0626f218fa8a172413b542cb",
+    "url": "/static/media/spotify.b1644e5d.png"
+  },
+  {
+    "revision": "52820f2b44eb8beb510e9a2039e1b287",
+    "url": "/static/media/video.52820f2b.mp4"
+  }
+]);
